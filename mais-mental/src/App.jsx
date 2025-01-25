32d5ec3f08c773f73be489406843d4c258635dc9
pedrohenriqux <pedrohenriquemacielldesousa@gmail.com>
2025-01-25 10:39:37 -0300

    criando botao

--- mais-mental/src/App.jsx
+++ mais-mental/src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+    </div>
+  )
 }
 
 export default App;
---
+++ mais-mental/src/components/BtnAjuda/BtnAjuda.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BtnAjuda = () => {
+  return (
+    <div>
+        <button
+        className='bg-primary hover:bg-third text-zinc-100 font-bold py-3 px-3 rounded-xl uppercase'>
+            procurar ajuda agora
+        </button>
+    </div>
+  )
+}
+
+export default BtnAjuda;

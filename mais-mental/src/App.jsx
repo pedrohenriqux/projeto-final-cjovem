51854414656a7ca0e506cc13b9e3648b7eaf35be
pedrohenriqux <pedrohenriquemacielldesousa@gmail.com>
2025-01-26 15:33:26 -0300

    criando o diretório pages e inserindo rotas

--- mais-mental/src/App.jsx
+++ mais-mental/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 

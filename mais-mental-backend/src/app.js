59ba284f1bc61de3e95347fc2299c98527b6ac40
pedrohenriqux <pedrohenriquemacielldesousa@gmail.com>
2025-03-19 21:10:55 -0300

    adicionando a rota para os grupos de apoio

--- mais-mental-backend/src/app.js
+++ mais-mental-backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const depoimentoRoutes = require('./routes/depoimentoRoutes');
+const grupoApoioRoutes = require('./routes/grupoApoioRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use('/depoimentos', depoimentoRoutes);
+app.use('/grupo-de-apoio', grupoApoioRoutes);
 
 module.exports = app;

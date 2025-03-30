4962768a0a683d0a71b2596e6ded417356cea68d
pedrohenriqux <pedrohenriquemacielldesousa@gmail.com>
2025-03-30 01:10:36 -0300

    criando a page 'Admin' e adicionando sua rota no arquivo App.jsx

--- mais-mental/src/App.jsx
+++ mais-mental/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Home.jsx';
 import Login from './pages/Login/Login';
 import SouPsicologo from './pages/SouPsicologo/SouPsicologo.jsx';
+import Admin from './pages/Admin/Admin.jsx';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />}/>
       <Route path='/SouPsicologo' element={<SouPsicologo />}/>
+      <Route path='/admin' element={<Admin />}/>
     </Routes>
     </BrowserRouter>
   )
